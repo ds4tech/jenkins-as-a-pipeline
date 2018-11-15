89f3e49fc4eda095d5f5b05e02f7dcbf098c2fd6
Szymon Homa <s2lomon@gmail.com>
2018-11-15 02:13:40 +0100

    adding pipeline-dashboard nested-view

--- src/main/groovy/com/szymonhoma/pipelinedsl/view/ServiceViewBuilder.groovy
+++ src/main/groovy/com/szymonhoma/pipelinedsl/view/ServiceViewBuilder.groovy
@@ -3,6 +3,7 @@ package com.szymonhoma.pipelinedsl.view
 import com.szymonhoma.pipelinedsl.ServiceProject
 import com.szymonhoma.pipelinedsl.Steps
 import javaposse.jobdsl.dsl.DslFactory
+import javaposse.jobdsl.dsl.views.ListView
 
 class ServiceViewBuilder {
 
@@ -26,6 +27,48 @@ class ServiceViewBuilder {
                 }
                 pipelines {
                     component("Trigger pipeline for ${project.qualifiedName}", Steps.BUILD_AND_PUBLISH.getJobName(project))
+                }
+            }
+        }
+
+        dslFactory.nestedView('pipelines-dashboard') {
+            views {
+                listView("production-deployments") {
+                    jobs {
+                        for (ServiceProject project : projects) {
+                            name(Steps.DEPLOY_TO_PRODUCTION.getJobName(project))
+                        }
+                    }
+                    columns {
+                        columns {
+                            name()
+                            weather()
+                            status()
+                            lastDuration()
+                            lastBuildConsole()
+                        }
+                    }
+                }
+                nestedView("pipeline execution aggregated") {
+                    views {
+                        for (ServiceProject project : projects) {
+                            deliveryPipelineView("${project.qualifiedName}-aggregated-pipeline") {
+                                showAggregatedPipeline()
+                                pipelineInstances(0)
+                                columns(1)
+                                updateInterval(5)
+                                showAvatars()
+                                showChangeLog()
+                                configure {
+                                    (it / linkRelative).value = true
+                                    (it / showStaticAnalysisResults).value = true
+                                }
+                                pipelines {
+                                    component("Aggregated pipeline for ${project.qualifiedName}", Steps.BUILD_AND_PUBLISH.getJobName(project))
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
